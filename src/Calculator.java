12b7958d2e954c69d1fa31a6bc2630c2ff122f52
Stefan Ieremie <Stefan.Ieremie@mail.bcu.ac.uk>
2018-11-05 12:33:18 +0000

    Lab work

---
+++ src/Calculator.java
@@ -0,0 +1,52 @@
+import java.util.Stack;
+
+public class Calculator {
+	
+	Stack<Integer> stack = new Stack<>();
+	
+	public String toString() {
+		if( stack.isEmpty()) {
+			return "[].";
+		}
+		else {
+			for(int i = 1; i < stack.size(); i++) {
+				if
+			}
+		}
+	}
+	
+	public void loadConstant(int i) {
+		stack.push(i);
+		
+		
+	}
+
+	
+
+	public void multiply() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void subtract() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void divide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void total() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
